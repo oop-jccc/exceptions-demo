9f6e88d602033e839f6b3fa25e755b7bc845aab2
dgerman <damon.german@perceptivesoftware.com>
2018-09-04 14:40:20 -0500

    Added more test.

---
+++ TryParseExamples/ExceptionFilters.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TryParseExamples
+{
+    [TestClass]
+    public class ExceptionFilterTests
+    {
+        [TestMethod]
+        public void ExceptionFilterTest() //??
+        {
+            string result = null;
+            try
+            {
+                new WebClient().DownloadString("https://google.com/");
+            }
+            catch (WebException ex) when (ex.Status == WebExceptionStatus.Timeout)
+            {
+                result = "Timeout!";
+            }
+            catch (WebException ex) when (ex.Status == WebExceptionStatus.NameResolutionFailure)
+            {
+                result = "Name resolution failure!";
+            }
+            catch (WebException ex)
+            {
+                result = $"Some other failure: {ex.Status}";
+            }
+
+            Assert.AreEqual(null, result);
+        }
+
+    }
+}
