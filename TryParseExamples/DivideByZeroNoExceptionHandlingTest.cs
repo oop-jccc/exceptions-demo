3ec0cd16d5f965c33ff44b4bcdc2e82a7063ac32
dgerman <damon.german@perceptivesoftware.com>
2018-09-04 14:06:40 -0500

    Added more examples

---
+++ TryParseExamples/DivideByZeroNoExceptionHandlingTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TryParseExamples
+{
+    [TestClass]
+    public class DivideByZeroNoExceptionHandlingTests
+    {
+        [TestMethod]
+        public void DivideByZeroNoExceptionHandlingTest()
+        {
+            var success = true;
+            var numerator = 5;
+            var denominator = 0;
+
+            try
+            {
+                var badResult = numerator / denominator;
+            }
+            catch (DivideByZeroException e)
+            {
+                success = false;
+            }
+            finally
+            {
+                Assert.IsFalse(success);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivideByZeroUnhandledExceptionHandlingTest()
+        {
+            var numerator = 5;
+            var denominator = 0;
+            var badResult = numerator / denominator;
+        }
+    }
+}
---
+++ TryParseExamples/NullableTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TryParseExamples
+{
+    [TestClass]
+    public class NullableTests
+    {
+        [TestMethod]
+        public void NullCoalescingTest() //??
+        {
+            string s1 = null;
+            var s2 = s1 == null ? "nothing" : s1;
+            Assert.AreEqual("nothing", s2);
+
+            var s3 = s1 ?? "nothing";
+            Assert.AreEqual("nothing", s3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void PropertyAccessErrorTest()
+        {
+            StringBuilder sb = null;
+            var s1 = sb.Length;
+        }
+
+        [TestMethod]
+        public void NullConditionalTest() //?.
+        {
+            StringBuilder sb = null;
+            var s = sb?.ToString().ToUpper();
+            var length = s?.Length;
+
+            Assert.AreEqual(0, length.GetValueOrDefault());
+        }
+
+        [TestMethod]
+        public void PropertyAccessHappyPathTest()
+        {
+            var sb2 = new StringBuilder("hello world!");
+            var s2 = sb2?.ToString().ToUpper();
+            Assert.AreEqual("HELLO WORLD!", s2);
+            Assert.AreEqual(12, s2?.Length);
+        }
+
+        [TestMethod]
+        public void NullCoalescingNullConditionalTest() // ?? smd ?.
+        {
+            StringBuilder sb = null;
+            var s = sb?.ToString().ToUpper() ?? "nothing"; 
+            Assert.AreEqual("nothing", s);
+        }
+    }
+}
