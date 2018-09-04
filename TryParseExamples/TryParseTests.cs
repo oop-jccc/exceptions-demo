e3933e35cdd95863fa93b7f550511885bbc0ddb4
dgerman <damon.german@perceptivesoftware.com>
2018-09-04 10:24:26 -0500

    Added TryParseTest

---
+++ TryParseExamples/TryParseTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TryParseExamples
+{
+    [TestClass]
+    public class TryParseTests
+    {
+        [TestMethod]
+        public void TryParseDoubleSuccessTest()
+        {
+            const string value = "3.14129";
+            var success = double.TryParse(value, out var result);
+
+            Assert.IsTrue(success);
+            Assert.AreEqual(3.14129, result, 0.000001);
+        }
+
+        [TestMethod]
+        public void TryParseDoubleFailTest()
+        {
+            const string value = "abcd";
+            var success = double.TryParse(value, out var result);
+
+            Assert.IsFalse(success);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
