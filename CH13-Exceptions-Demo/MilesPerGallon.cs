9fd1e82532de4c4b655187822a08a38a6c10f53d
dgerman <damon.german@perceptivesoftware.com>
2018-09-06 12:03:16 -0500

    Inclass examples

---
+++ CH13-Exceptions-Demo/MilesPerGallon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH13_Exceptions_Demo
+{
+  public class MilesPerGallon
+    {
+        private double _gallons;
+        private double _miles;
+
+        public double Mpg => Miles / Gallons;
+
+        public double Miles
+        {
+            get => _miles;
+            set
+            {
+                //ValidateNonNegative(value);
+                _miles = value;
+            }
+        }
+
+        public double Gallons
+        {
+            get => _gallons;
+            set
+            {
+                //ValidateNonNegative(value);
+                //ValidateNonZero(value);
+                _gallons = value;
+            }
+        }
+    }
+}
--- CH13-Exceptions-Demo/Program.cs
+++ CH13-Exceptions-Demo/Program.cs
@@ -10,6 +10,67 @@ namespace CH13_Exceptions_Demo
     {
         static void Main(string[] args)
         {
+            //Unhandeled();
+            //Handeled();
+            //TryParse();
+            ThrowExample();
         }
+
+        private static void Unhandeled()
+        {
+            Console.WriteLine("Enter and integer");
+            var input = Console.ReadLine();
+            var output = int.Parse(input);
+            Console.WriteLine(output);
+        }
+
+        private static void Handeled()
+        {
+            try
+            {
+                Unhandeled();
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("Thank you");
+            }
+        }
+
+        private static void TryParse()
+        {
+            Console.WriteLine("Enter and integer");
+            var input = Console.ReadLine();
+            bool success = int.TryParse(input, out var output);
+
+            if (success)
+            {
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine("You did something wrong.");
+            }
+
+        }
+        private static void ThrowExample()
+        {
+            Console.WriteLine("Enter and integer");
+            var input = Console.ReadLine();
+            var output = int.Parse(input);
+            if(output <0) throw new NegativeNumberException();
+        }
+
     }
 }
